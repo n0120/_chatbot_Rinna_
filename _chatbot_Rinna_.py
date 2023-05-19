784a9dc3cfd95a24e5236fe8f2378398fc5b5d90
n0120 <84952528+n0120@users.noreply.github.com>
2023-05-19 15:26:23 +0900

    Create _chatbot_Rinna_.py

---
+++ _chatbot_Rinna_.py
@@ -0,0 +1,36 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("rinna/japanese-gpt-1b", use_fast=False)
+model = AutoModelForCausalLM.from_pretrained("rinna/japanese-gpt-1b")
+
+def chatbot_response(input_text):
+    # ユーザーからの入力をエンコード
+    input_ids = tokenizer.encode(input_text, return_tensors='pt')
+    
+    # モデルによるテキスト生成
+    with torch.no_grad():
+        output_ids = model.generate(
+            input_ids,
+            max_length=100,
+            min_length=20,
+            do_sample=True,
+            top_k=50,
+            top_p=0.95,
+            pad_token_id=tokenizer.pad_token_id,
+            bos_token_id=tokenizer.bos_token_id,
+            eos_token_id=tokenizer.eos_token_id,
+            bad_words_ids=[[tokenizer.unk_token_id]]
+        )
+    
+    # 生成されたテキストをデコード
+    output_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+
+    return output_text
+
+while True:
+    user_input = input("あなた: ")
+    if user_input.lower() == "quit":
+        break
+    response = chatbot_response(user_input)
+    print("チャットボット: ", response)
